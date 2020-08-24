c2a6d813f65205a2bbb55ee2c592bfcaa955b325
Sekizo <urfill21@gmail.com>
2020-08-24 17:24:01 +0300

    wp config repairing

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// import "./style.css";
-import "./pages/index.css" // test
-// import img from "../PW9/images/avatar.jpg";  // test
-// import img from "../PW9/images/close.svg";  // test
-// import img from "../PW9/images/like-active.svg";  // test
-// import img from "../PW9/images/like-inactive.svg";  // test
-// import img from "../PW9/images/logo.svg";  // test
-// import img from "../PW9/images/trash-icon.svg";  // test
